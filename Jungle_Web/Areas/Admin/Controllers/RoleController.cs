7527a36ad17d2bd45d7563aebe52bc669b38369c
a <a@a.ca>
2022-10-31 11:09:52 -0400

    added role controller

---
+++ Jungle_Web/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,82 @@
+﻿using Jungle_DataAccess;
+using Jungle_Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jungle_Web.Areas.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly Jungle_DataAccess.JungleDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(JungleDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return View();
+            }
+            else
+            {
+                //update
+                var objFromDb = _db.Roles.FirstOrDefault(u => u.Id == id);
+                return View(objFromDb);
+            }
+
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(IdentityRole roleObj)
+        {
+            if (await _roleManager.RoleExistsAsync(roleObj.Name))
+            {
+                //error
+                // MESSAGE
+                return RedirectToAction(nameof(Index));
+            }
+            if (string.IsNullOrEmpty(roleObj.Id))
+            {
+                //create
+                await _roleManager.CreateAsync(new IdentityRole() { Name = roleObj.Name });
+                // MESSAGE
+            }
+            else
+            {
+                //update
+                var objRoleFromDb = _db.Roles.FirstOrDefault(u => u.Id == roleObj.Id);
+                if (objRoleFromDb == null)
+                {
+                    //MESSAGE
+                    return RedirectToAction(nameof(Index));
+                }
+                objRoleFromDb.Name = roleObj.Name;
+                objRoleFromDb.NormalizedName = roleObj.Name.ToUpper();
+                var result = await _roleManager.UpdateAsync(objRoleFromDb);
+                // MESSAGE
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+    }
+
+
+}
